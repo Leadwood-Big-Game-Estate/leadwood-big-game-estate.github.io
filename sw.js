b7b7f74e2c7f9ca4e63e1b903585223a3b75bc16
leopoldlangen <leopoldlangen@gmail.com>
2026-09-19 10:17:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *  Un chemin absolu ("/index.html") pointerait vers la racine du domaine,
  *  addAll() échouerait et l'installation entière serait annulée.
  * ------------------------------------------------------------------ */
-const VERSION = "v12";
+const VERSION = "v14";
 const CACHE   = "leadwood-" + VERSION;
 
 const PRECACHE = [
